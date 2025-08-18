e804c32ee4a8c31fe4a7d0336f7d369f05d3ac87
unknown <lucasdepaula.sk2@gmail.com>
2025-08-18 16:44:11 -0300

    Feat: interface responsiva

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -1,7 +1,10 @@
+
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/ui/core/UIComponent"
-], function (Controller, UIComponent) {
+    "sap/ui/core/UIComponent",
+    "sap/ui/core/routing/History",  
+      "sap/m/MessageToast"
+], function (Controller, UIComponent, History,MessageToast) {
     "use strict"
     return Controller.extend("sap.ui.demo.walkthrough.controller.Detail", {
         onInit: function () {
@@ -13,6 +16,22 @@ sap.ui.define([
                 path: "/" + window.decodeURIComponent(oEvent.getParameter("arguments").invoicePath),
                 model: "invoice"
             })
+        },
+        onNavBack: function () {
+            var oHistory = History.getInstance();
+            var sPreviousHash = oHistory.getPreviousHash();
+
+            if (sPreviousHash !== undefined) {
+                window.history.go(-1)
+            } else {
+                var oRouter = UIComponent.getRouterFor(this);
+                oRouter.navTo("overview", {}, true)
+            }
+        }, onRatingChange: function (oEvent) {
+            var fValue = oEvent.getParameter("value");
+            var oResourceBundle = this.getView().getModel("i18n").getResourceBundle();
+
+            MessageToast.show(oResourceBundle.getText("ratingConfirmation", [fValue]));
         }
     })
 })
